b14da7d6d0cb06e427e965d0edf480f019c978a8
waqarshaikh <shaikhwaqar1999@gmail.com>
2025-08-03 11:45:47 -0400

    add env var

--- webapp/settings.py
+++ webapp/settings.py
@@ -1,8 +1,9 @@
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'replace-this-with-a-secure-secret-key'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 DEBUG = True
 

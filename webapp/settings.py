5467b1544afba4201017dd8a401f427d2a529646
waqarshaikh <shaikhwaqar1999@gmail.com>
2025-08-03 12:27:56 -0400

    add local env

--- webapp/settings.py
+++ webapp/settings.py
@@ -1,7 +1,11 @@
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load .env file variables into environment
+load_dotenv(BASE_DIR / '.env')
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 if not SECRET_KEY:
